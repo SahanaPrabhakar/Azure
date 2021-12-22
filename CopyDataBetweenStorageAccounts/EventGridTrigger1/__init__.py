7fe97c659c5b4551c3705812d721e381223b8a20
Sahana Prabhakar <sahanasp80@gmail.com>
2021-12-22 20:42:01 +0000

    Update __init__.py

--- CopyDataBetweenStorageAccounts/EventGridTrigger1/__init__.py
+++ CopyDataBetweenStorageAccounts/EventGridTrigger1/__init__.py
@@ -10,7 +10,6 @@ from azure.storage.blob import BlobLeaseClient
 from azure.storage.blob import BlobServiceClient
 from azure.storage.blob import ContainerClient
 import azure.functions as func
-import time
 
 def main(event: func.EventGridEvent):
     result = json.dumps({
@@ -33,34 +32,46 @@ def main(event: func.EventGridEvent):
         logging.info(f'Found Summary.txt in blob name')     
         tmp = blobname.split("/")
         foldername = tmp[0]        
-        copyContainer(containername, foldername)
+        copy_container(containername, foldername)
 
 
-def copyContainer(container_name, foldername):    
+def copy_container(container_name, foldername):    
         
     blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AZURE_STORAGE_CONNECTION_STRING"))
     
     # Instantiate a ContainerClient
-    container_client = blob_service_client.get_container_client(container_name)    
-    sourceBlobList = container_client.list_blobs()
-    for blob in sourceBlobList:
-       b_name = blob.name              
-       if(b_name.__contains__(foldername)):
-           logging.info(f'Initiaite copy : {b_name}')
-           blob_copy(container_name, b_name)
+    container_client = blob_service_client.get_container_client(container_name)   
+    # Get a list of all blobs.  
+    sourceBlobList = container_client.list_blobs(foldername)        
+    # TODO: Add pagination
 
-def wait_for_copy(blob):
-    count = 0
-    props = blob.get_blob_properties()
-    while props.copy.status == 'pending':
-        count = count + 1
-        if count > 50:
-            raise TimeoutError('Timed out waiting for async copy to complete.')
-        #time.sleep(60)
-        #Testing time = 2s. Change later.
-        time.sleep(2)
-        props = blob.get_blob_properties()
-    return props
+    try:    
+        count = 0
+        for blob in sourceBlobList:
+            b_name = blob.name              
+            logging.info(f'Initiaite copy : {b_name}')        
+            count = count+1
+            blob_copy(container_name, b_name)
+
+        #Write new file to destination blob with number of files to wait for before deleting source
+        logging.info(f'Number of files: {str(count)}')
+        data = str(count).encode(encoding = 'UTF-8', errors = 'strict')
+
+        # Create a BlobClient representing the destination blob with a unique name.
+        target_container_name = os.getenv("AZURE_TARGET_CONTAINER")
+        target_file_name = foldername + "/Number_of_files.txt"
+
+        dest_blob = BlobClient.from_connection_string(
+                os.getenv("AZURE_STORAGE_DEST_CONNECTION_STRING"),
+                target_container_name, target_file_name
+                )
+
+        # Write number of files that will be copied to destination blob/target_file_name
+        dest_blob.upload_blob(data, blob_type="BlockBlob")     
+
+    except Exception as e:
+        logging.exception(f"Function copy_container Exception: {e}")
+
 
 def blob_copy(container_name, blob_name):
 
@@ -68,16 +79,11 @@ def blob_copy(container_name, blob_name):
         os.getenv("AZURE_STORAGE_CONNECTION_STRING"), 
         container_name, blob_name
         )
-
-    #logging.info('source blob BlobClient object created')
+    
     try:
-        # Lease the source blob for the copy operation
-        # to prevent another client from modifying it.
-        lease = BlobLeaseClient(source_blob)    
-        lease.acquire()
 
-        # Get the source blob's properties and display the lease state.
-        source_props = source_blob.get_blob_properties()
+        # No lease required as source blob will not change anymore
+        
         # Create a BlobClient representing the
         # destination blob with a unique name.
         target_container_name = os.getenv("AZURE_TARGET_CONTAINER")
@@ -86,18 +92,8 @@ def blob_copy(container_name, blob_name):
             target_container_name, blob_name
             )
 
-        # Start the copy operation.
-        dest_blob.start_copy_from_url(source_blob.url)
-        
-
-        # Get the destination blob's properties to check the copy status.            
-        wait_copy_data = wait_for_copy(dest_blob)
-        logging.info(f'Copy status for {blob_name} is: {wait_copy_data.copy["status"]}')
-
-        if (source_props.lease.state == "leased"):
-            # Break the lease on the source blob.
-            lease.break_lease(lease_break_period=1)
-            #source_blob.delete_blob(lease=lease)
+        # Start the async copy operation.
+        dest_blob.start_copy_from_url(source_blob.url)        
         
     except Exception as e:
         logging.exception(f"Function blob_copy Exception: {e}")
