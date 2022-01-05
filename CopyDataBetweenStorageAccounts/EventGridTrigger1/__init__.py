5cf0f357092baaf549ffd909b1c1886ecec2bf38
Sahana Prabhakar <sahanasp80@gmail.com>
2022-01-05 17:37:37 +0000

    Update __init__.py

--- CopyDataBetweenStorageAccounts/EventGridTrigger1/__init__.py
+++ CopyDataBetweenStorageAccounts/EventGridTrigger1/__init__.py
@@ -1,8 +1,6 @@
 import json
 import logging
 import os
-import uuid
-import sys
 import azure.core
 from azure.storage import blob
 from azure.storage.blob import BlobClient
@@ -10,6 +8,7 @@ from azure.storage.blob import BlobLeaseClient
 from azure.storage.blob import BlobServiceClient
 from azure.storage.blob import ContainerClient
 import azure.functions as func
+from azure.identity import ManagedIdentityCredential, ChainedTokenCredential
 
 def main(event: func.EventGridEvent):
     result = json.dumps({
@@ -32,49 +31,51 @@ def main(event: func.EventGridEvent):
         logging.info(f'Found Summary.txt in blob name')     
         tmp = blobname.split("/")
         foldername = tmp[0]        
-        copy_container(containername, foldername)
+        copy_container( containername, foldername)
 
 
 def copy_container(container_name, foldername):    
-        
-    blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AZURE_STORAGE_CONNECTION_STRING"))
-    
-    # Instantiate a ContainerClient
-    container_client = blob_service_client.get_container_client(container_name)   
-    # Get a list of all blobs.  
-    sourceBlobList = container_client.list_blobs(foldername)            
+    try:
+        # Get a token credential for authentication
+   
+        managed_identity = ManagedIdentityCredential()
+        credential_chain = ChainedTokenCredential(managed_identity)
+        # Instantiate a BlobServiceClient using a token credential    
+        source_account_url=os.getenv("SOURCE_ACCOUNT_URL")
+        source_blob_service_client = BlobServiceClient(account_url=source_account_url, credential=credential_chain)          
+            
+        # Instantiate a ContainerClient
+        source_container_client = source_blob_service_client.get_container_client(container_name)   
+        # Get a list of all blobs.  
+        sourceBlobList = source_container_client.list_blobs(foldername)      
 
-    try:    
+        target_account_url = os.getenv("TARGET_ACCOUNT_URL")
+        target_container_name = os.getenv("TARGET_CONTAINER_NAME")
+        target_container_client = ContainerClient(account_url=target_account_url, container_name=target_container_name,credential=credential_chain)
+        
         count = 0
+        # For loop will run through paginated response of ItemPaged
         for blob in sourceBlobList:
             b_name = blob.name              
             logging.info(f'Initiaite copy : {b_name}')        
             count = count+1
-            blob_copy(container_name, b_name)
+            blob_copy(source_container_client, b_name, target_container_client)
 
     except Exception as e:
         logging.exception(f"Function copy_container Exception: {e}")
 
 
-def blob_copy(container_name, blob_name):
+def blob_copy(container_client, blob_name, target_container_client):
 
-    source_blob = BlobClient.from_connection_string(
-        os.getenv("AZURE_STORAGE_CONNECTION_STRING"), 
-        container_name, blob_name
-        )
+    source_blob = container_client.get_blob_client(blob_name)    
     
     try:
 
         # No lease required as source blob will not change anymore
         
-        # Create a BlobClient representing the
-        # destination blob with a unique name.
-        target_container_name = os.getenv("AZURE_TARGET_CONTAINER")
-        dest_blob = BlobClient.from_connection_string(
-            os.getenv("AZURE_STORAGE_DEST_CONNECTION_STRING"),
-            target_container_name, blob_name
-            )
-
+        # Create a BlobClient 
+        dest_blob = target_container_client.get_blob_client(blob_name)                   
+        
         # Start the async copy operation.
         dest_blob.start_copy_from_url(source_blob.url)        
         
